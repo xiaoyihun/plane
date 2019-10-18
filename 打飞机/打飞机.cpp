9f4a14e2c4b8a2f76b874ac256dfc6ef2b83bb54
Zhang <1531288277@qq.com>
2019-10-18 20:43:06 +0800

    添加项目打飞机

---
+++ 打飞机/打飞机.cpp
@@ -0,0 +1,40 @@
+﻿#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#include<string.h>
+#include<stdlib.h>
+
+#include<graphics.h>
+#include<easyx.h>
+
+//引用Windows Multimedia API
+#pragma comment(lib,"Winmm.lib")
+
+#define High 579 // 游戏画面尺寸
+#define Width 1024
+
+
+
+/*
+	1.初始画游戏界面 
+		void startMenu()
+	2.显示画面
+*/
+
+IMAGE img_bk;//背景图片
+
+
+void startMenu()
+{
+	putimage(0, 0, &img_bk);
+	setbkmode(TRANSPARENT);
+	settextcolor(BLACK);
+	settextstyle(50, 0, _T("黑体"));
+	outtextxy(Width * 0.3, High * 0.2,"1 新游戏");
+	
+}
+
+int main()
+{
+    
+}
